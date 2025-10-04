74e03193eed6e15983eccefebfac3adc29d78370
KrishaThakor27IT <23it132@charusat.edu.in>
2025-10-04 11:53:13 +0530

    Testing my CI/CD pipeline

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World! This is my CI-CD Node.js Application.');
+  res.status(200).send('Hello, 23IT132! This is my CI-CD Node.js Application.');
 });
 
 app.get('/status', (req, res) => {
